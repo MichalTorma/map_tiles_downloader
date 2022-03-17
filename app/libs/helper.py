fdcd5cd6f5c00024dfa2a9c7b4634832b4dc200e
Michal Torma <michal.torma@nhm.uio.no>
2022-03-17 13:11:57 +0100

    Fix path

--- app/libs/helper.py
+++ app/libs/helper.py
@@ -47,7 +47,7 @@ def get_url():
 
 
 def generate_tiles(zoom: int, bounds_string: str):
-    tiles_path = Path('~/app/tiles/')
+    tiles_path = Path('output/tiles')
     tiles_path.mkdir(parents=True, exist_ok=True)
     tiles_file = tiles_path / 'tiles.geojson'
     command = f"echo \"{bounds_string}\" | mercantile tiles {zoom} | mercantile shapes > {tiles_file.as_posix()}"
